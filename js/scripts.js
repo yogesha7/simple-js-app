5e44d12b3fe2bbc15bf210233e9dba2089f4dbdb
yogesha7 <yogeshacharya71@gmail.com>
2022-03-24 00:00:47 -0400

    Update scripts.js
    Added a for loop statement to the code.

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,19 @@
     name: 'Pidgey',
     height: 0.3,
     type: ['flying', 'normal']
+},
+
+{
+    name: 'Charizard',
+    height: 5.07,
+    type: ['flame']
 }
 
  ]
+
+ for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 5) {
+    console.log('WOW! this Pokemon is big')
+    document.write(pokemonList[i].name + '(height : ' + pokemonList[i].height  +')' + 'WOW! this Pokemon is big! </br>')
+    }
+}
